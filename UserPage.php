06ac425c2308be255b53f497b37d7446a20d749d
Tadhg O'Rourke <orourketadhg@gmail.com>
2018-11-25 14:00:22 +0000

    display 5 per page on table in progress - offset bug - string to conversion bug

--- UserPage.php
+++ UserPage.php
@@ -7,7 +7,7 @@
 	    // open database connection
         $db = mysqli_connect('localhost:3307', 'root', '', 'LibraryDB');
 		
-		echo "In Session";
+		//echo "In Session";
 
 		//SQL required for showing all books
 		$bookSQL = "SELECT ISBN, BookTitle, Author, Edition, Year, CategoryID, Reserved FROM Book";
@@ -20,14 +20,20 @@
 
         }
 		else {
-            echo "<table border=1>";
 
             //turn $books into a 3 dimensional array
             //Dimension 1 : groups of 5 books
             //Dimension 2 : one of the 5 books in Dimension 1
             //Dimension 3 : access data piece of one books in Dimension 2
-            $bookArray = Array(Array());
 
+            $bookArray = Array(Array(Array()));
+            $bookArraySQL = Array();
+
+
+            while($row = mysqli_fetch_row($books)) {
+                $bookArraySQL[] = $row;
+
+            }
 
             //loop through groups of 5
             for($i = 0; $i < 2; $i++) {
@@ -37,6 +43,7 @@
 
                     //add book instance data to BookArray
                     for($k = 0; $k < 7; $k++) {
+                        $bookArray[$i][$j][$k] = $bookArraySQL[$k];
 
 
                     }
@@ -45,30 +52,51 @@
 
             }
 
-		    while ($row = mysqli_fetch_row($books)) {
-                echo "<tr><td>";
-                echo ($row[0]);
-                echo "</td><td>";
-                echo ($row[1]);
-                echo "</td><td>";
-                echo ($row[2]);
-                echo "</td><td>";
-                echo ($row[3]);
-                echo "</td><td>";
-                echo ($row[4]);
-                echo "</td><td>";
-                echo ($row[5]);
-                echo "</td><td>";
-                echo ($row[6]);
-                echo "</td></tr>";
+//            print_r($bookArray);
+
+            echo "<table border=1>";
+
+
+
+            $i = 0;
+
+            for($j = 0; $j < 5; $j++) {
+
+                echo "<tr>";
+
+                for($k = 0; $k < 7; $k++){
+                    echo "<td>";
+                    echo ($bookArray[$i][$j][$k]);
+                    echo "</td>";
+                }
+
+                echo "</tr>";
+
             }
 
+//		    while ($row = mysqli_fetch_row($books)) {
+//                echo "<tr><td>";
+//                echo ($row[0]);
+//                echo "</td><td>";
+//                echo ($row[1]);
+//                echo "</td><td>";
+//                echo ($row[2]);
+//                echo "</td><td>";
+//                echo ($row[3]);
+//                echo "</td><td>";
+//                echo ($row[4]);
+//                echo "</td><td>";
+//                echo ($row[5]);
+//                echo "</td><td>";
+//                echo ($row[6]);
+//                echo "</td></tr>";
+//            }
+
+            echo "</tr>";
+
             echo "</table>";
+
         }
-
-
-
-
 
 		if(isset($_POST['Logout'])){
 			$result = $_POST['Logout'];
