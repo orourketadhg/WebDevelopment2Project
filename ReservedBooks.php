8c9acd7d881e80d2c84ceed82bf2112fd13af3c4
Tadhg O'Rourke <orourketadhg@gmail.com>
2018-11-25 20:08:15 +0000

    Working on giving Users Reserved Books

--- ReservedBooks.php
+++ ReservedBooks.php
@@ -34,16 +34,65 @@
         $db = mysqli_connect('localhost:3307', 'root', '', 'LibraryDB');
 
         //get sessions username
-        $user = $_SESSION['username'];
+        $user = $_SESSION['Username'];
 
         // SQL required for Reserved Books page
-        $userReservedSQl = "SELECT ISBN, BookTitle, Author, Year, CategoryID FROM Book JOIN Reserved ON (ISBN) WHERE Username = '$user';";
+        //Todo : Fix foreign key bug in linking books and reserved
+        $userReservedSQl = "SELECT ISBN, BookTitle, Author, Year, CategoryID, Reserved FROM Book JOIN Reserved  (ISBN) WHERE Username = '$user';";
 
         $SQLResult = mysqli_query($db, $userReservedSQl);
 
         if (!$SQLResult) {
             echo "Database Failure: Error Code 1";
 
+        }
+        else {
+
+            $bookArraySQL = Array();
+
+            // add each row array to a array (2D array)
+            while($row = mysqli_fetch_row($SQLResult)) {
+                $bookArraySQL[] = $row;
+
+            }
+
+            echo "<table border=1>";
+
+            // Table Column names
+            echo "<tr>";
+            echo "<th>ISBN</th>";
+            echo "<th>Book Title</th>";
+            echo "<th>Author</th>";
+            echo "<th>Edition</th>";
+            echo "<th>Year</th>";
+            echo "<th>Category ID</th>";
+            echo "<th>Reserved</th>";
+            echo "</tr>";
+
+            for ($i = 0; $i < count($bookArraySQL); $i++) {
+
+                echo "<tr>";
+
+                //loop through each book printing its data
+                for ($j = 0; $j < 7; $j++) {
+                    if ($j != 8) {
+                        echo "<td>";
+                        echo $bookArraySQL[$i][$j];
+                        echo "</td>";
+                    }
+                    else {
+                        echo "<td>";
+                        echo '<button type="button">Reserve</button>';
+                        echo "</td>";
+                    }
+
+                }
+
+                echo "</tr>";
+
+            }
+
+            echo "</table>";
         }
 
 
